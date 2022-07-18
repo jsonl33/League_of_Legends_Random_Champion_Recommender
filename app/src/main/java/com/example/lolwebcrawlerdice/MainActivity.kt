37a69d43c30777d487755f39477d97852c72ca18
thisIsHyeongSeokLee <oddvapor@gmail.com>
2022-07-18 15:14:58 +0900

    trying Jsoup to parse html from LoL website

--- app/src/main/java/com/example/lolwebcrawlerdice/MainActivity.kt
+++ app/src/main/java/com/example/lolwebcrawlerdice/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.lolwebcrawlerdice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import org.jsoup.Jsoup
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+
+        }
     }
-}
